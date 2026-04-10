d13e800dfea71c6ff8c23ec6eacebc7bb9ae81cf
peembk <your.email@example.compeembk.28@gmail.com>
2026-04-10 13:53:03 +0700

    config cartfixture and import addedProduct from cartfixture

--- tests/cart.page.spec.ts
+++ tests/cart.page.spec.ts
@@ -1,5 +1,9 @@
-import { test, Page, expect } from '@playwright/test'
+import { Page, expect } from '@playwright/test'
 import { CartPage } from '../page/cart.page'
 import { ProductPage, products } from '../page/product.page'
+import { test } from '../fixtures/cart.fixture'
 
 
+test('', async ({ addedProduct }) => {
+
+})
